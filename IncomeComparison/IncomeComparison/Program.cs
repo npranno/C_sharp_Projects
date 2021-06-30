224580d34b073a9a1609d9a1f5265d2c7729f761
npranno <77467876+npranno@users.noreply.github.com>
2021-06-30 12:05:22 -0700

    Create IncomeComparison

---
+++ IncomeComparison/IncomeComparison/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+            Console.WriteLine("Person 1\nHourly Rate?");
+            Console.ReadLine();
+            int person1Hourly = 15;
+            Console.WriteLine(person1Hourly);
+            Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            Console.ReadLine();
+            int person1Hours = 40;
+            Console.WriteLine(person1Hours);
+            Console.ReadLine();
+            Console.WriteLine("Person 2\nHourly Rate?");
+            Console.ReadLine();
+            int person2Hourly = 20;
+            Console.WriteLine(person2Hourly);
+            Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            Console.ReadLine();
+            int person2Hours = 40;
+            Console.WriteLine(person2Hours);
+            Console.ReadLine();
+            Console.WriteLine("Annual Salary of Person 1:");
+            int person1Salary = ((person1Hourly * person1Hours) * 52);
+            Console.ReadLine();
+            Console.WriteLine(person1Salary);
+            Console.ReadLine();
+            Console.WriteLine("Annual Salary of Person 2:");
+            int person2Salary = ((person2Hourly * person2Hours) * 52);
+            Console.ReadLine();
+            Console.WriteLine(person2Salary);
+            Console.ReadLine();
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.ReadLine();
+            bool isMore = person1Salary > person2Salary;
+            Console.WriteLine(isMore);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MathOperations/MathOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+            //Console.ReadLine();
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemp = 70;
+            //int currentTemp = 72;
+            //bool isWarm = currentTemp > roomTemp;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
