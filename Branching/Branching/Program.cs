2dd22e681395373ee4def15daeb686b2f3cc0360
npranno <77467876+npranno@users.noreply.github.com>
2021-06-30 15:08:49 -0700

    Create Branching Assignment

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to package express. Please follow the instructions below.\n");
+            Console.WriteLine("What is the weight of your package in pounds?");
+            int packageWeight = (Convert.ToInt32(Console.ReadLine()));
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Your package is too heavy to ship via Package Express. Have a good day\nHit any key to end.");
+                Console.ReadKey();
+                return;
+                
+            }
+
+            Console.WriteLine("What is the height of your package in inches?");
+            double packageHeight = (Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("What is the length of your package in inches?");
+            double packageLength = (Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("What is the length of your package in inches?");
+            double packageWidth = (Convert.ToDouble(Console.ReadLine()));
+
+            if (packageHeight + packageLength + packageWidth> 50)
+            {
+                Console.WriteLine("Your package is too big to be shipped via Package Express\nHit any key to end.");
+                Console.ReadKey();
+                return;
+            }
+
+            double quote = ((packageLength + packageHeight + packageWidth) * packageWeight / 100);
+
+            Console.WriteLine("Your shipping total is $" + quote + ".");
+            Console.ReadKey();
+            return;
+        }
+    }
+}
---
+++ PracticeOperators/PracticeOperators/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = 25;
+            //int num2 = 35;
+
+            //if (num1 == num2)
+            //{
+            //    Console.WriteLine("num1 equals num2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("num1 does not equal num2");
+            //}
+            //Console.ReadLine();
+
+            //int roomTemp = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemp = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (currentTemp < roomTemp)
+            //{
+            //    Console.WriteLine("it is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ummmmmmmm.");
+            //}
+            //Console.ReadLine();
+
+
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 24 ? "your favorite number is even" : "your favorite number is odd";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
