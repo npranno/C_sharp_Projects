ffe5ff1e71ddf5d45b839b59dc2d8c97a8d40a25
npranno <77467876+npranno@users.noreply.github.com>
2021-07-01 09:45:48 -0700

    Create ConsoleApp(DoWhile)

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = string.Empty;
+            do
+            {
+                Console.WriteLine("Please enter your password:");
+                password = Console.ReadLine();
+            }
+            while (password != "TheTechAcademy");
+          
+
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
