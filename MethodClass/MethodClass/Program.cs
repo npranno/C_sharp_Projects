4485b0d669b0a6f167cf8be813d4f5ac2d3e598a
npranno <77467876+npranno@users.noreply.github.com>
2021-07-17 13:31:00 -0700

    MethodClass

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class_method class_Method = new class_method(); //instantiate class 'class_method'
+            Console.WriteLine("Please enter two whole numbers -- One to multiply by 5, and one to print to the screen.");
+
+            //this try catch sequence catches any user errors such as formatting
+            try
+            {
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                class_Method.numbers(num1: num1, num2: num2);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a whole number.");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ MethodClass/MethodClass/class_method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class class_method
+    {
+        public void numbers(int num1, int num2)
+        {
+            Console.WriteLine("Result of your first number multiplied by 5: " + num1 * 5);
+            Console.WriteLine("Your second number: " + num2);
+        }
+    }
+}
