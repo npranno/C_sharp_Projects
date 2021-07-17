8d8a5a80e2850a6fcf6655a209f5f1e2fe2478c5
npranno <77467876+npranno@users.noreply.github.com>
2021-07-17 12:38:41 -0700

    Method

---
+++ Method/Method/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            optional numbers = new optional(); //instantiates class 'optional'
+            Console.WriteLine("Please enter a whole number: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a new number. This is optional.\n" +
+                "If you would like to skip this, press Enter to see your total:");
+
+            int total; //declared from 'optional' class so Main() recognizes it
+            var finalInput = int.TryParse(Console.ReadLine(), out int input2); //bool
+
+            if (finalInput)
+            {
+                total = optional.parameters(input1, input2);
+            }
+            else
+            {
+                total = optional.parameters(input1);
+            }
+
+            Console.WriteLine("Your total is: " + total);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method/Method/optional.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class optional
+    {
+        public static int parameters(int one, int two = 0) //'int two' is optional so if the user only enters one input, it automatically sets to 0
+        {
+            int total = one + two;
+            return total;
+        }
+    }
+}
