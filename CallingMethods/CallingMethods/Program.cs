59b8c031bcbc1d27a3c3966b3acde8e0336304f8
npranno <77467876+npranno@users.noreply.github.com>
2021-07-17 10:40:45 -0700

    CallingMethods

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methods math = new methods();
+            Console.WriteLine("Please enter a number you would like to have\nAdded, Subtracted, Multiplied, and Divided by 5:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(input + " plus 5 is " + math.math1(input) + "."); //calls method and passes user input as a parameter
+            Console.WriteLine(input + " minus 5 is " + math.math2(input) + ".");
+            Console.WriteLine(input + " multiplied by 5 is " + math.math3(input) + ".");
+            Console.WriteLine(input + " divided by 5 is " + math.math4(input) + ".");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class methods
+    {
+        public int math1(int add) //public makes it usable throught the entire project
+        {
+            int total = add + 5;
+            return total;
+        }
+        public int math2(int subtract)
+        {
+            int total = subtract - 5;
+            return total;
+        }
+        public int math3(int multiply)
+        {
+            int total = multiply * 5;
+            return total;
+        }
+        public int math4(int divide)
+        {
+            int total = divide / 5;
+            return total;
+        }
+    }
+}
