722415f6d161b1d0e27c189cd4d94fbfc2ea078a
npranno <77467876+npranno@users.noreply.github.com>
2021-07-17 12:14:42 -0700

    MainMethod

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            method math = new method(); //calls class 'method'
+            Console.WriteLine("Passing in integer 5 and adding 5: " + math.number(5)); //calling a method by using class, method name, and parameters
+            Console.WriteLine("Passing in decimal 5.5 and multiplying by 5: " + math.number(5.5m));  //total is rounded to 30
+            Console.WriteLine("Passing in integer 6 as a string: " + math.number("6")); //total is rounded to 1
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethod/MainMethod/method.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class method
+    {
+        //multiple methods having the same name but different data types
+        public int number(int add) //int which uses whole numbers
+        {
+            int total1 = add + 5;
+            return total1;
+        }
+        public int number(decimal multiply) //decimal which is defined by using the 'm' symbol
+        {
+            int total2 = Convert.ToInt32(multiply) * 5; //ToInt32 converts string value to integer
+            return total2;
+        }
+        public int number(string divide) //string value
+        {
+            int total2 = Convert.ToInt32(divide) / 5;
+            return total2;
+        }
+    }
+}
