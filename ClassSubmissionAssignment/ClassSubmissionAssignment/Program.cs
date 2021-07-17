af7d169a55e851e323ce6712de03d938d2c30787
npranno <77467876+npranno@users.noreply.github.com>
2021-07-17 14:49:34 -0700

    ClassSubmissionAssignment

---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            overload_methods number = new overload_methods(); //access to *non-static* methods in 'overload_methods' class
+            Console.WriteLine("Please enter a number to divide it by 2:");
+
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine()); //user input
+                number.divide(input); //calls first method and divides user input by 2
+                overload_methods.divide(input, out input); //calls second method and divides user input by 2
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a whole Number.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/overload_methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class overload_methods
+    {
+        public void divide(int inNumber) //user input of number
+        {
+            int total = inNumber / 2;
+            Console.WriteLine(total); //outputs user input divided by 2
+        }
+        public static void divide(int inNum1, out int outNum2) //outputs user number, overload, uses 'out' to output user input /2
+        {
+            int total = outNum2 = inNum1 / 2;
+            Console.WriteLine(total);
+        }
+        public static class empty //declares static class
+        {
+
+        }
+    }
+}
