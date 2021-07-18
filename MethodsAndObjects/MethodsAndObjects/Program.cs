15c2371e2f77618818eef2d934b2d60c6004d8fd
npranno <77467876+npranno@users.noreply.github.com>
2021-07-18 09:30:42 -0700

    MethodsAndObjects

---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee() { FirstName = "Sample", LastName = "Student" }; //instantiate and initialize object
+
+            person.SayName(); //calls superclass method on employee object
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,7 +11,6 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            int timesShuffled = 0;
             deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
@@ -21,16 +20,5 @@ namespace TwentyOne
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
-
-
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
