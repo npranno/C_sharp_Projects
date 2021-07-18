55247f29854397eea52866b716c5ba02b7a6bc3b
npranno <77467876+npranno@users.noreply.github.com>
2021-07-18 13:26:57 -0700

    OverloadOperator

---
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Id == b.Id;
+        }
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return a.Id != b.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Employee Id = obj as Employee;
+            if (Id != null)
+            {
+                return Id == this;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empOne = new Employee();
+            empOne.Id = 01;
+
+            Employee empTwo = new Employee();
+            empTwo.Id = 02;
+
+            Console.WriteLine(empOne == empTwo);
+            Console.ReadLine();
+        }
+    }
+}
