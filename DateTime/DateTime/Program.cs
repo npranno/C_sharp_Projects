aaba3c9024ccd5433942f871dd265aba43a2598b
npranno <77467876+npranno@users.noreply.github.com>
2021-07-20 11:45:16 -0700

    DateTime

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;//current dateTime by using the .Now
+            Console.WriteLine("Current date and time: " + currentDateTime);//print current date and time to console
+            Console.WriteLine("Please enter a number.");
+            string number = Console.ReadLine();
+            DateTime futureDateTime = currentDateTime.AddHours(Convert.ToInt32(number));
+            Console.WriteLine("In " + number + " hours, it will be " + futureDateTime);
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO
 
 namespace TwentyOne
 {
     class Program
     {
         static void Main(string[] args)
-        {           
-            //deck.Shuffle(3);
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
+        {
+            string text = "here is some text";
+            File.WriteAllText(@"C:\users\npran\desktop\log.txt", text);
+
+            Console.WriteLine("Welcome to the Grand Hotel and Casion\nLet's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer== "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
         }
     }
 }
