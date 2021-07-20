b06cc0735a4ea7bd2263a57d9542525c2b9748b1
npranno <77467876+npranno@users.noreply.github.com>
2021-07-19 17:07:35 -0700

    StructAssignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number numberObject = new Number();
+            numberObject.Amount = 20.5m;
+
+            Console.WriteLine(numberObject.Amount);
+            Console.ReadLine();
+        }
+    }
+}
